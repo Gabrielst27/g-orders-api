7f04a1d6e31a2b49e08d68a7748c1c22924af06e
Gabriel Torres <gabrielst328@gmail.com>
2026-09-19 21:17:34 -0300

    feat: add bcrypt as external hash provider

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableVersioning({ type: VersioningType.URI });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.API_PORT!);
 }
 bootstrap();
--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/modules/shared/database/database.module';
+import { HashModule } from 'src/modules/shared/hash/hash.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HashModule],
 })
 export class SharedModule {}
